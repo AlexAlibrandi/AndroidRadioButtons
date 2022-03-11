62b141d741922a88a35221c3c74871f61679ebac
AlexAlibrandi <alexander_alibrandi@hcl.com>
2022-03-11 11:12:35 -0500

    initial creation commit

---
+++ app/src/main/java/com/android/radiobuttons/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.android.radiobuttons
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RadioButton
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var linearLayout: LinearLayout
+    lateinit var green : RadioButton
+    lateinit var yellow : RadioButton
+    lateinit var red : RadioButton
+    lateinit var change : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        linearLayout = findViewById(R.id.linear)
+        green = findViewById(R.id.green)
+        yellow = findViewById(R.id.yellow)
+        red = findViewById(R.id.red)
+        change = findViewById(R.id.buttonChange)
+
+        change.setOnClickListener {
+            if(green.isChecked) linearLayout.setBackgroundColor(Color.GREEN)
+            else if (yellow.isChecked) linearLayout.setBackgroundColor(Color.YELLOW)
+            else if(red.isChecked) linearLayout.setBackgroundColor(Color.RED)
+        }
+    }
+}
